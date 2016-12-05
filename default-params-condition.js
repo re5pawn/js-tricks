a0a2e3170d20b541c8d193aec1d99471c378dd60
re5pawn <s4ichulin@gmail.com>
2016-12-05 19:16:39 +0200

    add default params condition trick

---
+++ default-params-condition.js
@@ -0,0 +1,8 @@
+function animalSentenceFancy(animals = 'tigers',
+    animals2 = (animals === 'bears') ? 'sealions' : 'bears') {
+  return `Lions and ${animals} and ${animals2}! Oh my!`;
+}
+
+animalSentenceFancy('bears');
+// Output:
+// "Lions and bears and sealions. Oh my!"
