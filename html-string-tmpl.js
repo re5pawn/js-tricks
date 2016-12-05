9e7fea214005b5cc3014f5f24899a736df5241bb
re5pawn <s4ichulin@gmail.com>
2016-12-05 19:28:53 +0200

    add html-string generation trick

---
+++ html-string-tmpl.js
@@ -0,0 +1,22 @@
+const tmpl = (users) => {
+  let data = users.map(user => {
+    return `<li>${user.name}: ${user.age}</li>`;
+  }).join('');
+
+  return `<ul>${data}</ul>`;
+}
+
+let users = [
+  {name: 'John Doe', age: 34},
+  {name: 'Jane Smith', age: 45},
+  {name: 'Bob Snow', age: 27}
+];
+
+tmpl(users);
+
+// Output:
+// <ul>
+//   <li>John Doe: 34</li>
+//   <li>Jane Smith: 45</li>
+//   <li>Bob Snow: 27</li>
+// </ul>
