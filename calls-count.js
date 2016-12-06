0a272c79f8dd2485ea919bfb8ec21d860e4dd0e5
re5pawn <s4ichulin@gmail.com>
2016-12-06 17:50:34 +0200

    add function's calls count trick

---
+++ calls-count.js
@@ -0,0 +1,21 @@
+function callsCount(fn) {
+  return function() {
+    console.count('Count of calls for "' + fn.name + '"');
+    return fn.apply(null, arguments);
+  }
+}
+
+// Usage
+function sum(a, b) {
+  return a + b;
+}
+
+var _sum = callsCount(sum);
+
+_sum(1, 2);
+// before output: Count of calls for "sum": 1
+
+_sum(2, 3);
+// before output: Count of calls for "sum": 2
+
+// ... Count of calls for "sum": N
