7594856daea9a4172a8d5c73ff139bc4573c7c42
re5pawn <s4ichulin@gmail.com>
2016-12-06 17:52:50 +0200

    add functional inheritance trick

---
+++ functional-inheritance.js
@@ -0,0 +1,49 @@
+// some base class
+var Machine = (function() {
+  // private property
+  var enabled = false;
+
+  function Machine() {
+    this.enable = function() {
+      enabled = true;
+    };
+
+    this.disable = function() {
+      enabled = false;
+    };
+  }
+
+  Machine.prototype.getState = function() {
+    return enabled;
+  }
+
+  return Machine;
+})();
+
+var CoffeeMachine = (function() {
+  function CoffeeMachine(power) {
+    // inherit
+    Machine.call(this);
+    // or
+    // Machine.apply(this, arguments);
+
+    var waterAmount = 0;
+    this.setWaterAmount = function(amount) {
+      waterAmount = amount;
+    };
+  }
+
+  // If parent's mathods are in the prototype
+  // for get access to them:
+  CoffeeMachine.prototype = Object.create(Machine.prototype);
+  CoffeeMachine.prototype.constructor = Machine;
+
+  return CoffeeMachine;
+})();
+
+var coffeeMachine = new CoffeeMachine(10000);
+
+coffeeMachine.enable(); // parent's method
+coffeeMachine.setWaterAmount(100);
+coffeeMachine.disable(); // parent's method
+coffeeMachine.getState(); // parent's prototype method
