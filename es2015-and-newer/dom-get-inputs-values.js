8c3fdf04699ab61365dc340e12a9d7605a513fbf
re5pawn <s4ichulin@gmail.com>
2016-12-09 12:23:57 +0200

    add work with DOM trick

---
+++ es2015-and-newer/dom-get-inputs-values.js
@@ -0,0 +1,11 @@
+// HTML
+// <form>
+//   <input type="text" name="firstName" value="John">
+//   <input type="text" name="secondName" value="Doe">
+//   <input type="email" name="contactEmail" value="johndoe@email.me">
+// </form>
+
+let inputs = [...document.querySelectorAll('form input')];
+let [firstName, secondName, contactEmail] = inputs.map(i => i.value);
+
+console.log(firstName, secondName, contactEmail); // John Doe johndoe@email.me
