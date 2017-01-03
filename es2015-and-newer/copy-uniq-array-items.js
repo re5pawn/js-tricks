d2a22ff42409f4e15df4a5632d08b521424ca128
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-01-03 17:50:10 +0200

    add comment

--- es2015-and-newer/copy-uniq-array-items.js
+++ es2015-and-newer/copy-uniq-array-items.js
@@ -1,6 +1,7 @@
 let someObj = { 'a': 1 };
 let arr = [1, 2, 2, someObj, 3, 4, 4, someObj, 4, 5];
 
+// get only uniq elements using Set (collection of uniq values)
 let uniq = [...new Set(arr)];
 
 console.log(uniq); // [1, 2, someObj, 3, 4, 5]
