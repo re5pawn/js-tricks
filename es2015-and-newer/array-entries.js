ae70a24d767f09128807c60467141dc30e9edd80
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-01-03 12:22:56 +0200

    add more comments

--- es2015-and-newer/array-entries.js
+++ es2015-and-newer/array-entries.js
@@ -1,9 +1,10 @@
 const arr = ['a', 'b', 'c'];
 
 // arr.entries() returns an iterable over index-element pairs
+// get these through destructuring and use with for...of loop
 for (const [index, elem] of arr.entries()) {
   console.log(`index = ${index}, elem = ${elem}`);
-  // Output:
+  // output:
   // index = 0, elem = a
   // index = 1, elem = b
   // index = 2, elem = c
