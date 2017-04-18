b017a8b414fefa3277529129025aadc9d437d125
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 17:18:19 +0300

    Update dom-get-inputs-values.js

--- es2015-and-newer/dom-get-inputs-values.js
+++ es2015-and-newer/dom-get-inputs-values.js
@@ -1,11 +1,16 @@
-// HTML
-// <form>
-//   <input type="text" name="firstName" value="John">
-//   <input type="text" name="secondName" value="Doe">
-//   <input type="email" name="contactEmail" value="johndoe@email.me">
-// </form>
+/**
+ * HTML
+ * <form>
+ *  <input type="text" name="firstName" value="John">
+ *  <input type="text" name="secondName" value="Doe">
+ *  <input type="email" name="contactEmail" value="johndoe@email.me">
+ * </form>
+ */
 
 let inputs = [...document.querySelectorAll('form input')];
+// or
+// let inputs = Array.from(document.querySelectorAll('form input'));
+
 let [firstName, secondName, contactEmail] = inputs.map(i => i.value);
 
 console.log(firstName, secondName, contactEmail); // John Doe johndoe@email.me
