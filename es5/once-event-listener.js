7da69d5f203887c7f047f2e0c564e943f5626c46
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 16:33:04 +0300

    Update once-event-listener.js

--- es5/once-event-listener.js
+++ es5/once-event-listener.js
@@ -1,12 +1,9 @@
-element.addEventListener('click', function cb(event) {
+someElement.addEventListener('click', function cb(event) {
   console.log('clicked!');
-  // ...
-
   event.currentTarget.removeEventListener(event.type, cb);
 });
 
 // for modern browsers (http://caniuse.com/#feat=once-event-listener)
-element.addEventListener('click', function(event) {
+someElement.addEventListener('click', function(event) {
   console.log('clicked!');
-  // ...
 }, { once: true });
