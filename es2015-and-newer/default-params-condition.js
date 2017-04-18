a074631a64ebb296ca37d686edf2221f1dad7037
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 16:49:12 +0300

    Update default-params-condition.js

--- es2015-and-newer/default-params-condition.js
+++ es2015-and-newer/default-params-condition.js
@@ -1,8 +1,9 @@
-function animalSentenceFancy(animals = 'tigers',
-    animals2 = (animals === 'bears') ? 'sealions' : 'bears') {
+function animalSentenceFancy(
+  animals = 'tigers',
+  animals2 = (animals === 'bears') ? 'sealions' : 'bears'
+) {
   return `Lions and ${animals} and ${animals2}! Oh my!`;
 }
 
 animalSentenceFancy('bears');
-// Output:
-// "Lions and bears and sealions. Oh my!"
+// 'Lions and bears and sealions. Oh my!'
