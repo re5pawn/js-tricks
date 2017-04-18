6ccd7a0cf83071514bc0479dcf61ed17adc7bd70
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 16:31:15 +0300

    Update insert-css-rule.js

--- es5/insert-css-rule.js
+++ es5/insert-css-rule.js
@@ -1,19 +1,17 @@
-var sheet = (function() {
-  // Create the <style> tag
+var styleSheet = (function() {
   var style = document.createElement('style');
 
-  // Add a media (and/or media query) here if you'd like!
+  // Add a media (and/or media query) here if you'd like
   // style.setAttribute('media', 'screen')
   // style.setAttribute('media', 'only screen and (max-width : 1024px)')
 
-  // WebKit hack :(
+  // WebKit hack
   style.appendChild(document.createTextNode(''));
 
-  // Add the <style> element to the page
   document.head.appendChild(style);
 
   return style.sheet;
 })();
 
-// Usage
-sheet.insertRule("header { float: left; opacity: 0.8; }", 1);
+// Usage example
+styleSheet.insertRule("header { float: left; opacity: 0.8; }", 1);
