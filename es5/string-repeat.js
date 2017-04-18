e70f764e94ba475c1881abc8448c8dab7d7aa1ac
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 16:36:35 +0300

    Update string-repeat.js

--- es5/string-repeat.js
+++ es5/string-repeat.js
@@ -6,6 +6,7 @@ function repeatString(string, times) {
   return Array(times + 1).join(string);
 }
 
-repeatString('foo', 2); // foofoo
-repeatString('bar', 5); // barbarbarbarbar
-repeatString('baz', 1); // baz
+// Usage examples
+repeatString('foo', 2); // 'foofoo'
+repeatString('bar', 5); // 'barbarbarbarbar'
+repeatString('baz', 1); // 'baz'
