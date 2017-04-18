7013af2e72f6b12ea66075fa216d912cc4133fff
Sergey Chichulin <re5pawn@users.noreply.github.com>
2017-04-18 16:34:04 +0300

    Update random-id.js

--- es5/random-id.js
+++ es5/random-id.js
@@ -2,6 +2,5 @@ function randomId() {
   return Math.random().toString(36).slice(2);
 }
 
-randomId();
-// output something like this:
-// "lby760m7cv4bm90nmcxe6ogvi"
+// Usage example
+randomId(); // smth like - 'lby760m7cv4bm90nmcxe6ogvi'
