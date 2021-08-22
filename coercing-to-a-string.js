a7345e10d2cd6a35156983e87e1affb5b46561d7
Chetan Atrawalkar <66908113+ChetanAtrawalkarCA@users.noreply.github.com>
2021-08-22 17:03:03 +0530

    Create coercing-to-a-string.js
    We can quickly coerce a value to a string by wrapping it in a template literal.

---
+++ coercing-to-a-string.js
@@ -0,0 +1,11 @@
+// We can quickly coerce a value to a string by wrapping it in a template literal:
+
+const num = 2
+
+const numString = `${num}`
+
+// output:
+
+// {num: 2, numString: "2"}
+
+console.log({ num, numString })
